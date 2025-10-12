5c3b0fb2fbc34e271f0ff0028176c6480e5b4fab
L-Hadil <L-Hadil@users.noreply.github.com>
2025-10-12 19:55:14 +0200

    GUI

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,19 +2,38 @@ package app;
 
 import ui.CLI;
 import ui.GUI;
+import javax.swing.SwingUtilities;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-    	 CLI.start();
-       
-      /*  javax.swing.SwingUtilities.invokeLater(() -> {
-            try {
-                com.formdev.flatlaf.FlatLightLaf.setup();
-            } catch (Exception e) {
-                System.err.println("️ Thème FlatLaf non disponible, utilisation du thème par défaut.");
-            }
+        System.out.println("=== Analyseur de Code Java ===");
+        System.out.println("Choisissez le mode d’exécution :");
+        System.out.println("1. Interface Console (CLI)");
+        System.out.println("2. Interface Graphique (GUI)");
+        System.out.print("Votre choix : ");
 
-            new GUI().setVisible(true);
-        });*/
+        Scanner sc = new Scanner(System.in);
+        String choix = sc.nextLine().trim();
+
+        switch (choix) {
+            case "1":
+                CLI.start();
+                break;
+
+            case "2":
+                SwingUtilities.invokeLater(() -> {
+                    try {
+                        com.formdev.flatlaf.FlatLightLaf.setup();
+                    } catch (Exception e) {
+                        System.err.println("Thème FlatLaf non disponible, utilisation du thème par défaut.");
+                    }
+                    new GUI().setVisible(true);
+                });
+                break;
+
+            default:
+                System.out.println("Choix invalide. Fin du programme.");
+        }
     }
 }
